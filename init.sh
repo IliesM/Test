15f3dbb497ec794c5bd0486702334b35ad70fc62
Ilies Mulot <ilies@serveurcom.com>
2018-04-16 16:43:45 +0200

    Init

--- init.sh
+++ init.sh
@@ -2,16 +2,17 @@ apt-get update
 apt-get install -y composer rabbitmq-server git
 cd /home/
 git clone https://github.com/IliesM/Test.git InstaDm
+cd InstaDm
 service rabbitmq-server restart
 rabbitmq-plugins enable rabbitmq_management
 rabbitmqctl add_user admin admin
 rabbitmqctl set_user_tags admin administrator
 rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
-apt-get install php7.0-dev
-apt-get install php7.0-curl
-apt-get install php7.0-mbstring
-apt-get install php7.0-curl
-apt-get install php7.0-bcmath
-apt-get install php7.0-dom
-apt-get install php7.0-gd
+apt-get install -y php7.0-dev
+apt-get install -y php7.0-curl
+apt-get install -y php7.0-mbstring
+apt-get install -y php7.0-curl
+apt-get install -y php7.0-bcmath
+apt-get install -y php7.0-dom
+apt-get install -y php7.0-gd
 composer install
